25ad18f8e03ec6eaeed086a2d5b3c0e24161d252
Serge Trinquet <strinquet@cahpp.fr>
2017-07-28 16:26:30 +0200

    Modifs suite à mise en prod. sur serveur de test => modif code pour identification de l'utilisateur en prod. + affichage 'environnement'

--- app.js
+++ app.js
@@ -2,44 +2,38 @@ const express = require('express');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const dateFormat = require('dateformat');
+var logger = require('./log/logConfig.js').logger;
 const listHistoGrpController = require('./controllers/ListeHistoriqueGroupementsController'); // Custom module
 const CreateAccordController = require('./controllers/CreationAccordController'); // Custom module
 const RechAccordsController = require('./controllers/RechercheAccordsController'); // Custom module
 const ListeFacturesEtblController = require('./controllers/ListeFacturesEtblController'); // Custom module
-
 
 const app = express();
 
 /// Pour rendre les fonctionnalités de 'dateformat' accessibles dans ttes les pages .ejs, sous forme d'une variable locale
 app.locals.dateFormat = dateFormat;
 
-//set up template engine
+/// Set up template engine
 app.set('view engine', 'ejs');
 
 app.use(helmet()); /// Helmet helps you secure your Express apps 
 
 
 
-/*======== ESSAI POUR CONFIG =========*/
+/*==== Partie récupération de l'environnement ====*/
 //app.set('env', 'development'); //TEST au 20/06/17
 //app.locals.ENV = app.get('env');//TEST au 20/06/17
 console.log('Mode : ' + app.get('env')); //TEST le 20/06/17
 /***** Avec 'config' *****/
 const config = require("config"); //En TEST LE 21/06/17
 console.log('TEST sur module CONFIG: ' + config.get('env') + " | " + config.get('dbConfig.server') + " | " + config.get('dbConfig.database')); //TEST le 21/06/17
-/*======== FIN ESSAI POUR CONFIG =========*/
+/*==== Fin Partie récupération de l'environnement ====*/
 
-//static files
+/// Static files
 const env = app.get('env'); /// Pour détecter si on est en mode 'development' ou 'production', ou autre
 app.locals.ENV = env; // Pour être eccessible ds les .ejs
 app.use(express.static('./public/assets/' + (env === 'development' ? 'src' : 'dist')));
 
-
-/// Pour récupérer l'identifiant de l'utilisateur (va servir pour les droits)
-const path = require('path');
-var userName = process.env['USERPROFILE'].split(path.sep)[2];
-app.set('userName', userName || 'Non identifié');
-//var loginId = path.join("AD",userName);
 
 
 /// Placer la partie authentification après l'appel des static files car sinon ne trouve pas le chemin du .css pour 'AccesRefuse.ejs'
@@ -50,8 +44,7 @@ app.use(session.authentification);
 
 
 
-// Partie log (placé après l'appel des static files, sinon les enregistre)
-var logger = require('./log/logConfig.js').logger;
+///Partie log (placé après l'appel des static files, sinon les enregistre)
 app.use(require('winston-request-logger').create(logger, {
     'login': app.get('userName'),
     'method': ':method', 
