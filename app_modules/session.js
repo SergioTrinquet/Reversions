69cbe7e76b349c4bd378e7ef3f522d4f3a549af0
Serge Trinquet <strinquet@cahpp.fr>
2017-08-03 14:00:05 +0200

    Amélioration de l'interface pg 'Recherche des accords' pour le mode lecture

--- app_modules/session.js
+++ app_modules/session.js
@@ -2,7 +2,7 @@ const expressSession = require('express-session');
 
 module.exports = expressSession({
     cookie: {
-        expires: false //to enable the cookie to remain for only the duration of the user-agent
+        expires: false //Pour permettre au cookie d'exister juste le temps pendant lequel le navigateur est ouvert
     },
     secret: 'blargadeeblargblarg' // should be a large unguessable string
 });
