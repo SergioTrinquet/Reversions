39b609fd2ab46492505aa36cd47c3c87f3e43eb9
Serge Trinquet <strinquet@cahpp.fr>
2017-09-04 14:45:08 +0200

    Remplacement module 'express-session' par 'cookie-session' pour cause fuite memoire en prod avec express-session avec ma config

--- app_modules/session.js
+++ app_modules/session.js
@@ -1,8 +1,26 @@
+/*
 const expressSession = require('express-session');
+const FileStore = require('session-file-store')(expressSession); // Ajouté le 29/08/17
 
 module.exports = expressSession({
     cookie: {
         expires: false //Pour permettre au cookie d'exister juste le temps pendant lequel le navigateur est ouvert
     },
+    secret: 'blargadeeblargblarg', // should be a large unguessable string
+    store: new FileStore(
+        { 
+        path: "./TestFilesSession",
+        //reapInterval: 10, // En sec. 
+        }
+    ),resave: true,
+    saveUninitialized: false
+});
+*/
+
+
+// V2 : Avec cookie-session
+const cookieSession = require('cookie-session');
+module.exports = cookieSession({
+    name: 'cookieSession',
     secret: 'blargadeeblargblarg' // should be a large unguessable string
 });
